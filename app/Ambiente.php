fe1bafbb8ab4442f98bd244e4f448a7304bfb49b
joel5vega <joel5vega@gmail.com>
2020-11-18 16:28:45 -0400

    Se ocultaron los campos created_at

--- app/Ambiente.php
+++ app/Ambiente.php
@@ -7,20 +7,22 @@ use Illuminate\Database\Eloquent\Model;
 class Ambiente extends Model
 {
     //
-    protected $fillable = ['nombre','tipo','capacidad','edificio','piso'];
-
+    protected $fillable = ['nombre', 'tipo', 'capacidad', 'edificio', 'piso'];
+    protected $hidden = ["created_at", "updated_at"];
     public $timestamps = false;
 
-    public function scopeAulas($query){
-    return $query->where('tipo','=','aula');
+    public function scopeAulas($query)
+    {
+        return $query->where('tipo', '=', 'aula');
     }
-    public function scopeLabos($query){
-    return $query->where('tipo','=','laboratorio');
+    public function scopeLabos($query)
+    {
+        return $query->where('tipo', '=', 'laboratorio');
     }
     //scope para retornar los registros q coincidan con el tipo
-    
-    public function scopeTipo($query,$tipo){
-        return $query->where('tipo','=',$tipo);
+
+    public function scopeTipo($query, $tipo)
+    {
+        return $query->where('tipo', '=', $tipo);
     }
-    
 }
--- app/Responsable.php
+++ app/Responsable.php
@@ -8,7 +8,7 @@ class Responsable extends Model
 {
     //
     protected $fillable = ['nombre','ap_paterno','ap_materno','puesto','titulo','profesion','estado','ru'];
-    
+    protected $hidden = ["created_at", "updated_at"];
     public function scopeNivel($query,$nivel){
         return $query->where('puesto','=',$nivel);
     }
