9f476d9050f820d8043da92d539e39cb212ea107
joel5vega <joel5vega@gmail.com>
2022-03-03 12:48:50 -0400

    option

--- config/database.php
+++ config/database.php
@@ -62,7 +62,7 @@ return [
             'sslmode' => env('DB_SSLMODE','prefer'),
             'options' => (env('MYSQL_SSL') && extension_loaded('pdo_mysql')) ? [
                 // PDO::MYSQL_ATTR_SSL_KEY    => '/ssl/BaltimoreCyberTrustRoot.crt.pem',
-                PDO::MYSQL_ATTR_SSL_CA => "/home/site/wwwroot/ssl/BaltimoreCyberTrustRoot.crt.pem",
+                PDO::MYSQL_ATTR_SSL_CA => "/home/site/wwwroot/BaltimoreCyberTrustRoot.crt.pem",
             ] : []
             // 'options' => array(
             //     PDO::MYSQL_ATTR_SSL_CA => "/home/site/wwwroot/ssl/BaltimoreCyberTrustRoot.crt.pem",
